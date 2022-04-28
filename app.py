7c8223aa54ae93373fdab6e7a9621008c7c64a28
besapuz <aleksandzarinskij18772@gmail.com>
2022-04-29 01:38:01 +0300

    наладил работу post_list по get - запросу

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from blueprint.main import main
+from blueprint.loader import loader
+from functions import search_tag
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
 
+app.register_blueprint(main)
+app.register_blueprint(loader)
+
 
 @app.route("/")
 def page_index():
-    pass
+    return main
 
 
 @app.route("/list")
 def page_tag():
-    pass
+    get_content = request.args.get("s")
+    content = search_tag(get_content)
+    return render_template("post_list.html", content=content, get_content=get_content)
 
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_form():
     pass
+    #content = request.values.get("content")
 
 
 @app.route("/post", methods=["POST"])
@@ -32,5 +40,6 @@ def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(port=80)
 
--- functions.py
+++ functions.py
@@ -1,5 +1,24 @@
 import json
 
 
-def for_json():
-    pass
+def get_json_posts():
+    with open("posts.json", "r", encoding='utf8') as file:
+        dict_posts = json.load(file)
+    return dict_posts
+
+
+dict_json = get_json_posts()
+
+
+def search_tag(word):
+    if word:
+        list_content = []
+        s = ''.join(word.split())
+        for i in dict_json:
+            if s in i["content"]:
+                list_content.append(i)
+        return list_content
+    else:
+        return False
+
+
